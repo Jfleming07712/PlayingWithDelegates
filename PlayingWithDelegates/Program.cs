cb1ceaa699a13b23c7bb5ed29f75f9b0634da6c5
Jfleming <Jfleming07712@gmail.com>
2019-08-05 15:15:55 -0400

    Add project files.

---
+++ PlayingWithDelegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace PlayingWithDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> AddTwoNumbers = (x, y) => Console.WriteLine(x + y);
+
+            var calc = new Calculator();
+
+            calc.Calculate(3, 5, AddTwoNumbers);
+
+
+
+
+        }
+    public class Calculator
+        {
+            public void Calculate(int x, int y, Action<int, int> AddTwoNumbers)
+            {
+                AddTwoNumbers(x, y);
+                Console.WriteLine("math complete");
+
+            }
+        }
+        
+    }
+
+
+    
+}
